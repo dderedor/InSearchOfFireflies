7683303989d966de5938c895a9ea7927c4c5ba10
Mariya <kkddkk.dd@yandex.ru>
2025-05-08 20:38:07 +0300

    отрисовка травы, мерцание светлячка

--- mygame.py
+++ mygame.py
@@ -1,5 +1,6 @@
 #venv\Scripts\activate.bat
 import pygame
+import math
 import random
 
 pygame.init()
@@ -15,6 +16,8 @@ hedgehog_left_img = pygame.image.load('image/hedgehog_left.png')
 hedgehog_right_img = pygame.image.load('image/hedgehog_right.png')
 grass_img = pygame.image.load('image/trava.png')
 firefly_img = pygame.image.load('image/fly.png')
+leaf_img = pygame.image.load('image/leaf.png')
+#grib_img = pygame.image.load('image/.png')
 
 # Размеры экрана
 screen_width = screen.get_width()
@@ -25,8 +28,8 @@ tile_size = grass_img.get_width()
 
 # Ёжик
 hedgehog_x = 430 #  начальные координаты и скорость
-hedgehog_y = 600  #
-hedgehog_speed = 5
+hedgehog_y = 600  
+hedgehog_speed = 10
 current_hedgehog_img = hedgehog_down_img
 
 
@@ -34,16 +37,20 @@ current_hedgehog_img = hedgehog_down_img
 font = pygame.font.Font('fonts.ttf', 16) # Выбираем шрифт и размер (Arial, размер 36)
 
 
+korleaf = [
+    (20,100),
+    (150,300),
+    (60,550),
+    
+    (770,400),
+    (700,200),
+    (650,600),
 
+]
 
-# --- НОВЫЕ СТРОКИ: СВЕТЛЯЧКИ ---
 fireflies = [] # Список для хранения светлячков
-# --- УБРАЛИ: РАДИУС СБОРА СВЕТЛЯЧКОВ ---
-# --- НОВАЯ ПЕРЕМЕННАЯ: СЧЕТЧИК СВЕТЛЯЧКОВ ---
 collected_fireflies = 0 # Сколько светлячков собрано
-# --- НОВАЯ ПЕРЕМЕННАЯ: ТЕКУЩИЙ УРОВЕНЬ ---
 current_level = 1 # Текущий уровень
-# --- НОВЫЙ СЛОВАРЬ: ЦЕЛИ ПО УРОВНЯМ ---
 level_goals = {
     1: 3,
     2: 5,
@@ -77,12 +84,31 @@ while run:
         for x in range(0, screen_width, tile_size):
             screen.blit(grass_img, (x, y))
 
+
+
+
+
+
+    # 2. --- НАЧАЛО ВСТАВКИ ---
+    # Отрисовка светлячков с мерцанием
+    for firefly in fireflies:
+        firefly_copy = firefly_img.copy()  # Создаём копию изображения
+        alpha = 128+ int(128 * math.sin(pygame.time.get_ticks() * 0.003))  # Пульсация
+        firefly_copy.set_alpha(alpha)  # Применяем прозрачность
+        screen.blit(firefly_copy, (firefly[0], firefly[1]))  # Рисуем копию
+    # --- КОНЕЦ ВСТАВКИ ---
+
+
+
+
+
+
+
     # Отрисовка ёжика
     screen.blit(current_hedgehog_img, (hedgehog_x, hedgehog_y))
 
     # Отрисовка светлячков
-    for firefly in fireflies:
-        screen.blit(firefly_img, (firefly[0], firefly[1]))
+
 
     # --- ИЗМЕНЕНО: ОТОБРАЖЕНИЕ СЧЕТЧИКА СВЕТЛЯЧКОВ ---
     # --- ИЗМЕНЕНО: БЕЛЫЙ ЦВЕТ ТЕКСТА ---
@@ -127,6 +153,9 @@ while run:
             hedgehog_y += hedgehog_speed
         current_hedgehog_img = hedgehog_down_img
 
+    for i in korleaf:
+        screen.blit(leaf_img, i)
+
     # Ограничение движения ёжика
     hedgehog_x = max(0, min(hedgehog_x, screen_width - current_hedgehog_img.get_width()))
     hedgehog_y = max(0, min(hedgehog_y, screen_height - current_hedgehog_img.get_height()))
@@ -144,5 +173,5 @@ while run:
             start_new_level()
 
     pygame.display.update()
-
+    
 pygame.quit()
